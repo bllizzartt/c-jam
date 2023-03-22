a848bcb8e26a59542ee7fd855f19ad08705c4334
gamesbyaisha <nekoh150@newschool.edu>
2023-03-22 13:46:19 -0400

    Changed sprites

--- CuteRobot/Assets/AishaBikebayeva/Scripts/PetUIController.cs
+++ CuteRobot/Assets/AishaBikebayeva/Scripts/PetUIController.cs
@@ -7,7 +7,7 @@ namespace AishaGrowth
 {
     public class PetUIController : MonoBehaviour
     {
-        public Image foodImage, happinessImage, energyImage;
+        public Image foodImage, happinessImage, energyImage, musicImage, loveImage;
         public static PetUIController instance;
 
         private void Awake()
@@ -19,11 +19,13 @@ namespace AishaGrowth
             else Debug.LogWarning("more than 1 petuicontroller in scene");
         }
 
-        public void UpdateImages(int food, int happiness, int energy)
+        public void UpdateImages(int food, int happiness, int energy, int music, int love)
         {
             foodImage.fillAmount = (float) food/100;
             happinessImage.fillAmount = (float)happiness/100;
             energyImage.fillAmount = (float)energy/100;
+            musicImage.fillAmount = (float)music/100;
+            loveImage.fillAmount = (float)love/100;
             
         }
     }
